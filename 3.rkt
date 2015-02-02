b62e835344593c7fd63bd0aa7582e462ca87ae59
Tsgzj <wxsun1991@gmail.com>
2015-02-02 11:09:34 -0500

    Solution to HM#3

---
+++ 3.rkt
@@ -0,0 +1,172 @@
+;; ** The WAE interpreter
+;; Complete Coverage
+;; Adding sqrt to the language
+;; Multiple Values
+;; Fixing sqrt
+;; Fixing the Arithmetic Operators
+;; Fixing the With
+
+#lang pl 03
+
+#| BNF for the WAE language:
+     <WAE> ::= <num>
+             | { + <WAE> <WAE> }
+             | { - <WAE> <WAE> }
+             | { * <WAE> <WAE> }
+             | { / <WAE> <WAE> }
+             | { with { <id> <WAE> } <WAE> }
+             | <id>
+             | {sqrt <WAE>}
+|#
+
+;; WAE abstract syntax trees
+(define-type WAE
+  [Num  (Listof Number)]
+  [Add  WAE WAE]
+  [Sub  WAE WAE]
+  [Mul  WAE WAE]
+  [Div  WAE WAE]
+  [Id   Symbol]
+  [With Symbol WAE WAE]
+  [Sqrt WAE])
+
+(: parse-sexpr : Sexpr -> WAE)
+;; to convert s-expressions into WAEs
+(define (parse-sexpr sexpr)
+  (match sexpr
+    [(number: n)    (Num (list n))]
+    [(symbol: name) (Id name)]
+    [(cons 'with more)
+     (match sexpr
+       [(list 'with (list (symbol: name) named) body)
+        (With name (parse-sexpr named) (parse-sexpr body))]
+       [else (error 'parse-sexpr "bad `with' syntax in ~s" sexpr)])]
+    [(list '+ lhs rhs) (Add (parse-sexpr lhs) (parse-sexpr rhs))]
+    [(list '- lhs rhs) (Sub (parse-sexpr lhs) (parse-sexpr rhs))]
+    [(list '* lhs rhs) (Mul (parse-sexpr lhs) (parse-sexpr rhs))]
+    [(list '/ lhs rhs) (Div (parse-sexpr lhs) (parse-sexpr rhs))]
+    [(list 'sqrt hs) (Sqrt (parse-sexpr hs))]
+    [else (error 'parse-sexpr "bad syntax in ~s" sexpr)]))
+
+(: parse : String -> WAE)
+;; parses a string containing a WAE expression to a WAE AST
+(define (parse str)
+  (parse-sexpr (string->sexpr str)))
+
+#| Formal specs for `subst':
+   (`N' is a <num>, `E1', `E2' are <WAE>s, `x' is some <id>, `y' is a
+   *different* <id>)
+      N[v/x]                = N
+      {+ E1 E2}[v/x]        = {+ E1[v/x] E2[v/x]}
+      {- E1 E2}[v/x]        = {- E1[v/x] E2[v/x]}
+      {* E1 E2}[v/x]        = {* E1[v/x] E2[v/x]}
+      {/ E1 E2}[v/x]        = {/ E1[v/x] E2[v/x]}
+      {sqrt E}[v/x]         = {sqrt E[v/x]}
+      y[v/x]                = y
+      x[v/x]                = v
+      {with {y E1} E2}[v/x] = {with {y E1[v/x]} E2[v/x]}
+      {with {x E1} E2}[v/x] = {with {x E1[v/x]} E2}
+|#
+
+(: subst : WAE Symbol WAE -> WAE)
+;; substitutes the second argument with the third argument in the
+;; first argument, as per the rules of substitution; the resulting
+;; expression contains no free instances of the second argument
+(define (subst expr from to)
+  (cases expr
+    [(Num n) expr]
+    [(Add l r) (Add (subst l from to) (subst r from to))]
+    [(Sub l r) (Sub (subst l from to) (subst r from to))]
+    [(Mul l r) (Mul (subst l from to) (subst r from to))]
+    [(Div l r) (Div (subst l from to) (subst r from to))]
+    [(Sqrt n) (Sqrt (subst n from to))]
+    [(Id name) (if (eq? name from) to expr)]
+    [(With bound-id named-expr bound-body)
+     (With bound-id
+           (subst named-expr from to)
+           (if (eq? bound-id from)
+             bound-body
+             (subst bound-body from to)))]))
+
+#| Formal specs for `eval':
+     eval(N)         = N
+     eval({+ E1 E2}) = eval(E1) + eval(E2)
+     eval({- E1 E2}) = eval(E1) - eval(E2)
+     eval({* E1 E2}) = eval(E1) * eval(E2)
+     eval({/ E1 E2}) = eval(E1) / eval(E2)
+     eval({sqrt E})  = sqrt(eval(E))
+     eval(id)        = error!
+     eval({with {x E1} E2}) = eval(E2[eval(E1)/x])
+|#
+
+(: eval : WAE -> (Listof Number))
+;; evaluates WAE expressions by reducing them to numbers
+(define (eval expr)
+  (cases expr
+    [(Num n) n]
+    [(Add l r) (bin-op + (eval l) (eval r))]
+    [(Sub l r) (bin-op - (eval l) (eval r))]
+    [(Mul l r) (bin-op * (eval l) (eval r))]
+    [(Div l r) (bin-op / (eval l) (eval r))]
+    [(Sqrt e) (sqrt+ (eval e))]
+    [(With bound-id named-expr bound-body)
+     (eval (subst bound-body
+                  bound-id
+                  (Num (eval named-expr))))]
+    [(Id name) (error 'eval "free identifier: ~s" name)]))
+
+ (: sqrt+ : (Listof Number) -> (Listof Number))
+;; a version of `sqrt' that takes a list of numbers, and return a list
+;; with twice the elements, holding the two roots of each of the inputs;
+;; throws an error if any input is negative.
+(define (sqrt+ ns)
+  (cond [(null? ns) null]
+        [(< (first ns) 0)
+         (error 'eval "`sqrt' requires a non-negative input")]
+        [else (let ([n (sqrt (first ns))])
+                (cons n (cons (- 0 n) (sqrt+ (cdr ns)))))]))
+
+(: bin-op : (Number Number -> Number) (Listof Number) (Listof Number)
+            -> (Listof Number))
+;; applies a binary numeric function on all combinations of numbers from
+;; the two input lists, and return the list of all of the results
+(define (bin-op op ls rs)
+  (: helper : Number (Listof Number) -> (Listof Number))
+  (define (helper l rs)
+    (: f : Number -> Number)
+    (define (f rs) (op l rs))
+    (map f rs))
+  (if (null? ls)
+    null
+    (append (helper (first ls) rs) (bin-op op (rest ls) rs))))
+
+(: run : String -> (Listof Number))
+;; evaluate a WAE program contained in a string
+(define (run str)
+  (eval (parse str)))
+
+;; tests
+(test (run "5") => '(5))
+(test (run "{+ 5 5}") => '(10))
+(test (run "{+ {sqrt 1} 3}") => '(4 2))
+(test (run "{+ {/ {+ {sqrt 1} 3} 2} {sqrt 100}}") => '(12 -8 11 -9))
+(test (run "{sqrt {+ 16 {* {+ 1 {sqrt 1}} {/ 9 2}}}}") => '(5 -5 4 -4))
+(test (run "{with {x {+ 5 5}} {+ x x}}") => '(20))
+(test (run "{with {x 5} {+ x x}}") => '(10))
+(test (run "{with {x 5} {* x x}}") => '(25))
+(test (run "{with {x 5} {/ x x}}") => '(1))
+(test (run "{with {x 5} {sqrt {* x x}}}") => '(5 -5))
+(test (run "{with {x {+ 5 5}} {with {y {- x 3}} {+ y y}}}") => '(14))
+(test (run "{with {x 5} {with {y {- x 3}} {+ y y}}}") => '(4))
+(test (run "{with {x 5} {+ x {with {x 3} 10}}}") => '(15))
+(test (run "{with {x 5} {+ x {with {x 3} x}}}") => '(8))
+(test (run "{with {x 5} {+ x {with {y 3} x}}}") => '(10))
+(test (run "{with {x 5} {with {y x} y}}") => '(5))
+(test (run "{with {x 5} {with {x x} x}}") => '(5))
+(test (run "{with x 5 {with x x x}}")
+      =error> "bad `with' syntax in (with x 5 (with x x x))")
+(test (run "{null}") =error> "bad syntax in (null)")
+(test (run "{sqrt -5}") =error> "`sqrt' requires a non-negative input")
+(test (run "{with {x 1} y}") =error> "free identifier")
+
+(define minutes-spent 130)
