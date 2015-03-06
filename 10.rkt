3d1da8af1156ddc898127597922ca96cb4079588
Tsgzj <wxsun1991@gmail.com>
2015-03-05 22:44:51 -0500

    Sometimes bullshit is the answer 	modified:   10.rkt

--- 10.rkt
+++ 10.rkt
@@ -2,7 +2,7 @@
 ;; TODO Multiple Arguments part:
 ;;  - Flesh out rewrite
 ;;  - Write type definition for define/rec ackerman
-;;      
+;;
 ;;      Mutual Recursion part:
 ;;  - um, all of it :)
 
@@ -23,7 +23,7 @@
 ;                                     (ackermann m (- n 1)))])))))
 ;;; Second try
 ;(define ackermann
-;  (let ([g (Y (lambda (ackermann) 
+;  (let ([g (Y (lambda (ackermann)
 ;                (lambda (m n)
 ;                  (cond [(zero? m) (+ n 1)]
 ;                        [(zero? n) (ackermann (- m 1) 1)]
@@ -48,13 +48,40 @@
 (rewrite (define/rec (f x ...) E)
          => (define f
               (let ([g (Y (lambda (f)
-                            ...finish this part...))])
+                            (lambda (_)
+                              (lambda (x ...)
+                                (let ([f (f #f)])
+                                  E)))))])
                 (g #f))))
 
-;; TODO: Put in type definition
+;;(define ackermann
+;;  (let ([g (Y (lambda (ackermann)
+;;                (lambda (_) ; we ignore this argument
+;;                  (lambda (m n)
+;;                    (let ([ackermann (ackermann #f)])
+;;                        (cond [(zero? m) (+ n 1)]
+;;                              [(zero? n) (ackermann (- m 1) 1)]
+;;                              [else      (ackermann (- m 1)
+;;                                                    (ackermann m (- n 1)))]))))))])
+;;    (g #f)))
+
 (define/rec (ackermann m n)
   (cond [(zero? m) (+ n 1)]
         [(zero? n) (ackermann (- m 1) 1)]
         [else      (ackermann (- m 1) (ackermann m (- n 1)))]))
-
 (test (ackermann 3 3) => 61)
+
+(define/rec (fib a b count)
+  (if (zero? count)
+      b
+      (fib (+ a b) a (- count 1))))
+(test (fib 1 0 5) => 5)
+
+
+;; TODO: Put in type definition
+;;(define/rec (ackermann m n)
+;;  (cond [(zero? m) (+ n 1)]
+;;        [(zero? n) (ackermann (- m 1) 1)]
+;;        [else      (ackermann (- m 1) (ackermann m (- n 1)))]))
+
+;; (test (ackermann 3 3) => 61)
